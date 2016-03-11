16fe26e35484c3c0028ba1a7e78f9783aeccc79d
Kris Szafranski <kdszafranski@gmail.com>
2016-03-11 09:40:38 -0600

    fixed next call to next(). changed inline ifs to standard blocks

--- models/user.js
+++ models/user.js
@@ -13,10 +13,14 @@ var UserSchema = new Schema({
 UserSchema.pre('save', function(next) {
     var user = this;
 
-    if(!user.isModified('password')) return next;
+    if(!user.isModified('password')) {
+      return next();
+    }
 
     bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-        if(err) return next(err);
+        if(err) {
+          return next(err);
+        }
 
         bcrypt.hash(user.password, salt, function(err, hash) {
             if(err) return next(err);
