3a8889319023a2a533b70dd9583b52ff19d7a874
Kris Szafranski <kdszafranski@gmail.com>
2016-05-27 10:35:57 -0500

    renamed client app file, added ng-submit to login form, commented logout endpoint

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
+var path = require('path');
 
 // Handles Ajax request for user information if user is authenticated
 router.get('/', function(req, res) {
@@ -12,13 +13,15 @@ router.get('/', function(req, res) {
     res.send(req.user);
   } else {
     // failure best handled on the server. do redirect here.
-    // res.send(false);
     console.log('not logged in');
+    // res.sendFile(path.join(__dirname, '../public/views/index.html'));
     res.send(false);
   }
 });
 
+// clear all server session information about this user
 router.get('/logout', function(req, res) {
+  // Use passport's built-in method to log out the user
   req.logOut();
   res.sendStatus(200);
 });
