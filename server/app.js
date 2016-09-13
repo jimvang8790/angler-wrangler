8dd0bae086f21fc18f50a1617c04a15a067185ec
Kris Szafranski <kdszafranski@gmail.com>
2016-09-13 11:36:32 -0500

    updated for mlabs and heroku

--- server/app.js
+++ server/app.js
@@ -38,7 +38,18 @@ app.use('/user', user);
 app.use('/*', index);
 
 // Mongo Connection //
-var mongoURI = "mongodb://localhost:27017/passport";
+var mongoURI = '';
+// process.env.MONGODB_URI will only be defined if you
+// are running on Heroku
+if(process.env.MONGODB_URI != undefined) {
+    // use the string value of the environment variable
+    mongoURI = process.env.MONGODB_URI;
+} else {
+    // use the local database server
+    mongoURI = 'mongodb://localhost:27017/passport';
+}
+
+// var mongoURI = "mongodb://localhost:27017/passport";
 var mongoDB = mongoose.connect(mongoURI).connection;
 
 mongoDB.on('error', function(err){
